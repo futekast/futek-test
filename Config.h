e3ba9e92b3cb1fc1eabbe51174a09fca0b216a17
futekast <31968499+futekast@users.noreply.github.com>
2017-09-14 11:31:42 -0700

    init commit

---
+++ Config.h
@@ -0,0 +1,48 @@
+#pragma config FMIIEN = OFF             // Ethernet RMII/MII Enable (RMII Enabled)
+#pragma config FETHIO = ON              // Ethernet I/O Pin Select (Default Ethernet I/O)
+#pragma config PGL1WAY = ON             // Permission Group Lock One Way Configuration (Allow only one reconfiguration)
+#pragma config PMDL1WAY = ON            // Peripheral Module Disable Configuration (Allow only one reconfiguration)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+#pragma config FUSBIDIO = ON            // USB USBID Selection (Controlled by the USB Module)
+
+#pragma config FPLLICLK = PLL_FRC
+#pragma config FPLLIDIV = DIV_1
+#pragma config FPLLRNG = RANGE_5_10_MHZ
+#pragma config FPLLMULT = MUL_50
+#pragma config FPLLODIV = DIV_4
+#pragma config UPLLFSEL = FREQ_12MHZ
+
+// DEVCFG1
+#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
+#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disable SOSC)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+#pragma config POSCMOD = OFF            // Primary Oscillator Configuration (Primary osc disabled)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FCKSM = CSECME           // Clock Switching and Monitor Selection (Clock Switch Enabled, FSCM Enabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
+#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
+#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
+#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
+#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config TRCEN = ON               // Trace Enable (Trace features in the CPU are enabled)
+#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
+#pragma config FECCCON = OFF_UNLOCKED   // Dynamic Flash ECC Configuration (ECC and Dynamic ECC are disabled (ECCCON bits are writable))
+#pragma config FSLEEP = OFF             // Flash Sleep Mode (Flash is powered down when the device is in Sleep mode)
+#pragma config DBGPER = PG_ALL          // Debug Mode CPU Access Permission (Allow CPU access to all permission regions)
+#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable bit (MCLR pin generates a normal system Reset)
+#pragma config SOSCGAIN = GAIN_2X       // Secondary Oscillator Gain Control bits (2x gain setting)
+#pragma config SOSCBOOST = OFF          // Secondary Oscillator Boost Kick Start Enable bit (Normal start of the oscillator)
+#pragma config POSCGAIN = GAIN_1X       // Primary Oscillator Gain Control bits (1x gain setting)
+#pragma config POSCBOOST = OFF          // Primary Oscillator Boost Kick Start Enable bit (Normal start of the oscillator)
+#pragma config EJTAGBEN = NORMAL        // EJTAG Boot (Normal EJTAG functionality)
+
+// DEVCP0
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
---
+++ main.c
@@ -0,0 +1,63 @@
+#include "Config.h"
+#include <p32xxxx.h>
+#include <xc.h>
+#include <sys/attribs.h>
+
+#define F_CPU 100000000UL
+#define FCY F_CPU / 2
+#define TICK_HZ F_CPU / 2
+#define MS 1000
+#define US 1000000
+
+void DelayMS(unsigned long m)
+{
+    unsigned long stop = m * (TICK_HZ / MS) + _mfc0(_CP0_COUNT, _CP0_COUNT_SELECT);
+    while(_mfc0(_CP0_COUNT, _CP0_COUNT_SELECT) < stop);
+}
+
+void DelayUS(unsigned long u)
+{
+    unsigned long stop = u * (TICK_HZ / US) + _mfc0(_CP0_COUNT, _CP0_COUNT_SELECT);
+    while(_mfc0(_CP0_COUNT, _CP0_COUNT_SELECT) < stop);
+}
+
+void __ISR_AT_VECTOR(_EXTERNAL_1_VECTOR, IPL7SRS) ExternalInterrupt (void)
+{
+    IFS0bits.INT1IF = 0;
+    LATBbits.LATB11 ^= 1;
+    
+    LATAbits.LATA3 ^= 1;
+}
+
+void InitializeInterrupt()
+{
+    IPC2bits.INT1IP = 7; // Primary Priority
+    IPC2bits.INT1IS = 3; // Secondary Priority
+    INTCONbits.INT1EP = 1; // Rising Edge
+    IFS0bits.INT1IF = 0; // Reset Interrupt Flag
+    INTCONSET = _INTCON_MVEC_MASK;
+    IEC0bits.INT1IE = 1; // Enable Interrupt
+    
+    asm volatile("ei");
+
+    ANSELEbits.ANSE8 = 0; // Digital
+    TRISEbits.TRISE8 = 1; // Input
+
+    INT1R = 0x000D; //http://www.mouser.com/ds/2/268/60001320D-967555.pdf Page 252
+}
+
+int main(void)
+{
+    TRISBbits.TRISB11 = 0; // Output for LED
+    LATBbits.LATB11 = 0; // LED OFF
+    
+    InitializeInterrupt();
+
+    TRISAbits.TRISA3 = 0; // Output for testing
+    LATAbits.LATA3 = 0;
+    
+    while (1)
+    {
+    }
+    return 0;
+}
